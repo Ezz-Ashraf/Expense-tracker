2f7021428b0857bc59933b51dcd6c17d3c81c317
Ezz-Ashraf <ezzashraf056@gmail.com>
2022-12-03 21:06:06 +0200

    Added Expense Item component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,13 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
-    return (<div>
-        <h2>My First Component</h2>
-        <div>Thank you</div>
+    const itemDate = new Date(2022,10,22)
+    const itemDescription = 'Car Insurance'
+    const price = 470.25
+    return (<div className='Expense-Item-Container'>
+    <div className='Expense-Item-Date'>{itemDate.toISOString().slice(0,10)}</div>
+    <h2 className='Expense-Item-Description'>{itemDescription}</h2>
+    <div className='Expense-Item-Price'>${price}</div>
         </div>
     );
 }
