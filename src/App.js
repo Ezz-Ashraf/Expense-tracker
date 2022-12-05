21e70cd14edcde288993d5858b0561f4a4e01d98
Ezz-Ashraf <ezzashraf056@gmail.com>
2022-12-05 16:44:55 +0200

    Dynamic data added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import ExpenseItem from './components/ExpenseItem'
 
 function App() {
+  const expenses = [
+    {
+      id:'e1',
+      date:new Date(2022,10,22),
+      description:'Car Insurance',
+      price:202.75
+    },
+    {
+      id:'e2',
+      date:new Date(2021,5,22),
+      description:'Internet Money',
+      price:155.75
+    },
+    {
+      id:'e3',
+      date:new Date(2021,7,12),
+      description:'Food',
+      price:305.75
+    },
+    {
+      id:'e4',
+      date:new Date(2023,0,22),
+      description:'School Expenses',
+      price:202.75
+    }
+  ]
   return (
     <div>
  <h1>Hello World</h1>
- <ExpenseItem></ExpenseItem>
+ <ExpenseItem date = {expenses[0].date} id={expenses[0].id}description ={expenses[0].description} price ={expenses[0].price}></ExpenseItem>
+ <ExpenseItem date =  {expenses[1].date} id= {expenses[1].id} description = {expenses[1].description} price = {expenses[1].price}></ExpenseItem>
+ <ExpenseItem  date = {expenses[2].date} id ={expenses[2].id} description = {expenses[2].description} price = {expenses[2].price}></ExpenseItem>
+ <ExpenseItem  date ={expenses[3].date} id = {expenses[3].id} description = {expenses[3].description} price = {expenses[3].price}></ExpenseItem>
   </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,15 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-    const itemDate = new Date(2022,10,22)
-    const itemDescription = 'Car Insurance'
-    const price = 470.25
+function ExpenseItem(props) {
+    const month = props.date.toLocaleString('ar-EG',{month:'long'})
+    const dayOfWeek = props.date.toLocaleString('ar-EG',{weekday:'long'})
+    const dayOfMonth = props.date.toLocaleString('ar-EG',{day:'numeric'})
+    const itemDescription = props.description
+    const price =props.price
     return (<div className='Expense-Item-Container'>
-    <div className='Expense-Item-Date'>{itemDate.toISOString().slice(0,10)}</div>
+    <div className='Expense-Item-Date'>{month}</div>
+    <div className='Expense-Item-Date'>{dayOfWeek  +" - "+dayOfMonth}</div>
+    <div className='Expense-Item-Date'>{month}</div>
     <h2 className='Expense-Item-Description'>{itemDescription}</h2>
     <div className='Expense-Item-Price'>${price}</div>
         </div>
