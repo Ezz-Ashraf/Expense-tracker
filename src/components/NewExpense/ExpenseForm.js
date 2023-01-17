010891df2b13311ab1100621e1b0313e3b99738a
Ezz-Ashraf <ezzashraf056@gmail.com>
2023-01-18 00:09:37 +0200

    added multiple input states

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,8 +1,25 @@
+import  { useState } from 'react';
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  const inputChangeHandler = (event) => {
-    console.log(event.target.value);
+
+  const [ EnteredDescription , setDescription] = useState('');
+  const [ EnteredDate , setDate] = useState('');
+  const [ EnteredPrice , setPrice] = useState('');
+
+  const descriptionChangeHandler = (event) => {
+    setDescription(event.target.value)
+    console.log("Description = "+EnteredDescription);
+  };
+
+  const dateChangeHandler = (event) => {
+    setDate(event.target.value)
+    console.log("Date = "+EnteredDate);
+  };
+
+  const priceChangeHandler = (event) => {
+    setPrice(event.target.value)
+    console.log("Price = "+EnteredPrice);
   };
 
   return (
@@ -12,16 +29,17 @@ const ExpenseForm = () => {
         <input
           type="text"
           placeholder="Telephone Tax"
-          onChange={inputChangeHandler}
+          onChange={descriptionChangeHandler}
         ></input>
       </div>
       <div className="new-expense__control">
         <label>Expense Date</label>
-        <input type="date" min="2020-01-01" max="2024-12-31"></input>
+        <input type="date" min="2020-01-01" max="2024-12-31" onChange={dateChangeHandler}>
+        </input>
       </div>
       <div className="new-expense__control">
         <label>Expense Price</label>
-        <input type="number" min="0.1" step="0.3"></input>
+        <input type="number" min="0.1" step="0.3" onChange={priceChangeHandler}></input>
       </div>
     </form>
   );
