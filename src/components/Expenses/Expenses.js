4a318f367e909bd299119112799bcdef29a60d8c
Ezz-Ashraf <ezzashraf056@gmail.com>
2023-02-03 19:56:59 +0200

    Added Year Filtering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,17 +1,26 @@
+import React , {useState} from "react"
 import ExpenseItem from "./ExpenseItem" 
 import Card from "../UI/Card"
 import NewExpense from "../NewExpense/NewExpense"
+import ExpensesFilter from "./ExpenseFilter"
 import './Expenses.css'
 
 function Expenses(props) {
+
+const [filteredYear ,setFilteredYear] = useState('2020') 
 
 const  ExpenseHandler =(expenseData) => {
 props.onChangeExpenses(expenseData)
 }
 
+const FilterHandler =(selectedYear) => {
+    setFilteredYear(selectedYear)
+}
+
 return (
     <Card className="expenses">
 <NewExpense onAddExpenseHandler ={ExpenseHandler}></NewExpense>
+<ExpensesFilter defaultSelected={filteredYear} onChangeFilter ={FilterHandler}/>
          <ExpenseItem date = {props.expenses[0].date} id={props.expenses[0].id}description ={props.expenses[0].description} price ={props.expenses[0].price}></ExpenseItem>
  <ExpenseItem date =  {props.expenses[1].date} id= {props.expenses[1].id} description = {props.expenses[1].description} price = {props.expenses[1].price}></ExpenseItem>
  <ExpenseItem  date = {props.expenses[2].date} id ={props.expenses[2].id} description = {props.expenses[2].description} price = {props.expenses[2].price}></ExpenseItem>
